ae57adce999712646b0d1ce42aefe3e8c6feb417
John Liu <johnliu55tw@gmail.com>
2021-09-13 16:17:30 +0800

    Add a bit controller code; Run make build

--- todo/controllers/todo_controller.go
+++ todo/controllers/todo_controller.go
@@ -18,7 +18,12 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -47,9 +52,36 @@ type TodoReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
 func (r *TodoReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger := log.FromContext(ctx)
+	logger.V(1).Info("Reconcile func", "NamespacesName", req.NamespacedName)
 
-	// your logic here
+	/* 1. Load named Todo */
+	logger.V(1).Info("Loading Todo")
+	var todo todov1.Todo
+	if err := r.Get(ctx, req.NamespacedName, &todo); err != nil {
+		logger.Error(err, "unable to fetch Todo")
+		// we'll ignore not-found errors, since they can't be fixed by an immediate
+		// requeue (we'll need to wait for a new notification), and we can get them
+		// on deleted requests.
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+	logger.V(1).Info("Todo loaded", "todo", todo)
+
+	// TEST CREATE JOB
+	job, err := constructNotificationJob(&todo, time.Now())
+	logger.V(1).Info("Job created", "job", job)
+	if err != nil {
+		logger.Error(err, "failed to create job from todo item")
+		return ctrl.Result{}, nil
+	}
+
+	if err := r.Create(ctx, job); err != nil {
+		logger.Error(err, "unable to create Job for CronJob", "job", job)
+		return ctrl.Result{}, err
+	}
+	logger.V(1).Info("Notification job created")
+
+	/* 2. Update status from notification Job */
 
 	return ctrl.Result{}, nil
 }
@@ -59,4 +91,35 @@ func (r *TodoReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&todov1.Todo{}).
 		Complete(r)
+}
+
+func constructNotificationJob(todo *todov1.Todo, scheduledTime time.Time) (*batchv1.Job, error) {
+	// We want job names for a given nominal start time to have a deterministic name to avoid the same job being created twice
+	name := fmt.Sprintf("%s-%d", todo.Name, scheduledTime.Unix())
+
+	job := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Labels:      make(map[string]string),
+			Annotations: make(map[string]string),
+			Name:        name,
+			Namespace:   todo.Namespace,
+		},
+		Spec: batchv1.JobSpec{
+			Template: corev1.PodTemplateSpec{
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  fmt.Sprintf("%s-notify", name),
+							Image: "busybox",
+							Command: []string{"/bin/sh", "-c",
+								fmt.Sprintf("echo 'This is notification for %s'", todo.Spec.NotifyEmail),
+							},
+						},
+					},
+					RestartPolicy: corev1.RestartPolicyOnFailure,
+				},
+			},
+		},
+	}
+	return job, nil
 }
